c047673dae44b9fce4afe53cd954ef81876113f8
Ben Green <bengreenus@gmail.com>
2025-01-11 17:30:31 -0500

    reverting config passing

--- custom_components/wmata/coordinator.py
+++ custom_components/wmata/coordinator.py
@@ -1,6 +1,8 @@
 from .const import DEFAULT_SCAN_INTERVAL, HEADERS, URL
 from dataclasses import dataclass
 from datetime import timedelta
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_KEY, CONF_ID
 from homeassistant.core import DOMAIN, HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 import logging
@@ -23,13 +25,13 @@ class WmataCoordinator(DataUpdateCoordinator):
     
     data: APIData
 
-    def __init__(self, hass: HomeAssistant, api_key: str, station_id: str = "", stop_id: str = "") -> None:
+    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
         """Initialize coordinator class"""
         
         # Set variables from values entered in config flow setup
-        self.api_key = api_key
-        self.station = station_id
-        self.stop = stop_id
+        self.api_key = config_entry[CONF_API_KEY]
+        self.station = config_entry[CONF_ID]
+        self.stop = ""
         
         # TODO: do we need to confirm that the API works or is that covered in the config_flow?
         # doing it here anyways for now 
