7ef674afe4efed9253f6a178f36b55c99f005a2e
Ben Green <bengreenus@gmail.com>
2025-01-11 16:47:18 -0500

    combining classes, removing unused template data entries

--- custom_components/wmata/coordinator.py
+++ custom_components/wmata/coordinator.py
@@ -1,7 +1,6 @@
-from .const import DEFAULT_SCAN_INTERVAL, HEADERS, URL
+from .const import DEFAULT_SCAN_INTERVAL, HEADERS, URL, CONF_STATION_ID
 from dataclasses import dataclass
 from datetime import timedelta
-from enum import StrEnum
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY
 from homeassistant.core import DOMAIN, HomeAssistant
@@ -15,54 +14,29 @@ _LOGGER = logging.getLogger(__name__)
 
 
 @dataclass
-class WmataAPIData:
+class APIData:
     """Class to hold api data"""
     # TODO: modify to update when new data comes in on train distances
-
-    controller_name: str
-
-
-class DeviceType(StrEnum):
-    """Device types."""
-
-    TEMP_SENSOR = "temp_sensor"
-    DOOR_SENSOR = "door_sensor"
-    OTHER = "other"
-
-
-DEVICES = [
-    {"id": 1, "type": DeviceType.TEMP_SENSOR},
-    {"id": 2, "type": DeviceType.TEMP_SENSOR},
-    {"id": 3, "type": DeviceType.TEMP_SENSOR},
-    {"id": 4, "type": DeviceType.TEMP_SENSOR},
-    {"id": 1, "type": DeviceType.DOOR_SENSOR},
-    {"id": 2, "type": DeviceType.DOOR_SENSOR},
-    {"id": 3, "type": DeviceType.DOOR_SENSOR},
-    {"id": 4, "type": DeviceType.DOOR_SENSOR},
-]
-
-
-@dataclass
-class Device:
-    """API device."""
-
-    device_id: int
-    device_unique_id: str
-    device_type: DeviceType
-    name: str
-    state: int | bool
+    next_buses: list
+    next_trains: list
 
 
 class WmataCoordinator(DataUpdateCoordinator):
-    """My coordinator"""
-
-    data: WmataAPIData
+    """Base coordinator class"""
+    
+    data: APIData
 
     def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
-        """Initialize coordinator"""
-
+        """Initialize coordinator class"""
+        
         # Set variables from values entered in config flow setup
-        self.api_key = config_entry.data[CONF_API_KEY]
+        self.api_key: str = config_entry.data[CONF_API_KEY]
+        self.station: str = config_entry.data[CONF_STATION_ID]
+        
+        # TODO: do we need to confirm that the API works or is that covered in the config_flow?
+        # doing it here anyways for now 
+        self.connected: bool = False
+        self.async_validate_api_key(self.api_key)
 
         # set variables from options.  You need a default here incase options have not been set
         self.poll_interval = DEFAULT_SCAN_INTERVAL  # default to 1min
@@ -75,12 +49,25 @@ class WmataCoordinator(DataUpdateCoordinator):
             update_method=self.async_update_data,
             update_interval=timedelta(seconds=self.poll_interval),
         )
-
-        # initialize your api here
-        self.api = WmataAPI(api_key=self.api_key)
+        
+        # data formats:
+        # next_buses = [{"RouteID": "D6", "DirectionText": "South", "Minutes": 5}, ...]
+        # next_trains = [{"Destination": "Glenmont", "Line": "Red", "LocationName": "Glenmont", "Min": 3}, ...]
         self.next_buses = []
         self.next_trains = []
+        
+        # TODO: should be set by the user in the setup process
+        self.stop = str
         self.station = str
+        
+    async def async_validate_api_key(self) -> bool:
+        output = requests.get(
+            headers={"api_key": self.api_key}, url="https://api.wmata.com/Misc/Validate")
+
+        if output.status_code == 200:
+            self.connected = True
+            return True
+        raise APIAuthError("Invalid API key")
 
     async def async_update_data(self):
         """Fetch data from API endpoint.
@@ -89,13 +76,13 @@ class WmataCoordinator(DataUpdateCoordinator):
         so entities can quickly look up their data.
         """
 
-        # TODO: add API call here to get station info
-
         try:
-            if not self.api.connected:
-                await self.hass.async_add_executor_job(self.api.validate_api_key)
+            if not self.connected:
+                await self.hass.async_add_executor_job(self.async_validate_api_key)
 
-            devices = await self.hass.async_add_executor_job(self.api.get_devices)
+            # devices = await self.hass.async_add_executor_job(self.api.get_devices)
+            next_buses = await self.hass.async_add_executor_job(self.async_get_next_buses_at_stop, self.stop)
+            next_trains = await self.hass.async_add_executor_job(self.async_get_next_trains_at_station, self.station)
 
         except APIAuthError as err:
             _LOGGER.error(err)
@@ -106,9 +93,9 @@ class WmataCoordinator(DataUpdateCoordinator):
             raise UpdateFailed(f"Error communicating with API: {err}") from err
 
         # what is returned here is stored in self.data by the DataUpdateCoordinator
-        return WmataAPIData()
+        return APIData(next_buses=next_buses, next_trains=next_trains)
 
-    async def get_next_buses_at_stop(stop_code: str) -> list:
+    async def async_get_next_buses_at_stop(stop_code: str) -> list:
         output = requests.get(
             headers=HEADERS,
             url="%s/NextBusService.svc/json/jPredictions?StopID=%s" % (
@@ -117,17 +104,17 @@ class WmataCoordinator(DataUpdateCoordinator):
 
         bus_predictions = [bus for bus in output.json()["Predictions"]]
 
-        print(bus_predictions)
+        # print(bus_predictions)
 
-        for bus in bus_predictions:
-            print(bus["RouteID"])
-            print(bus["DirectionText"])
-            print(bus["Minutes"])
-            print()
+        # for bus in bus_predictions:
+        #     print(bus["RouteID"])
+        #     print(bus["DirectionText"])
+        #     print(bus["Minutes"])
+        #     print()
 
         return bus_predictions
 
-    async def get_next_trains_at_station(station_code: str) -> list:
+    async def async_get_next_trains_at_station(station_code: str) -> list:
         output = requests.get(
             headers=HEADERS,
             url="%s/StationPrediction.svc/json/GetPrediction/%s" % (
@@ -137,81 +124,25 @@ class WmataCoordinator(DataUpdateCoordinator):
         train_predictions = [train["Destination"]
                              for train in output.json()["Trains"]]
 
-        print(train_predictions)
+        # print(train_predictions)
 
-        for train in train_predictions:
-            print(train["Destination"])
-            print(train["Line"])
-            print(train["LocationName"])
-            print(train["Min"])
-            print()
+        # for train in train_predictions:
+        #     print(train["Destination"])
+        #     print(train["Line"])
+        #     print(train["LocationName"])
+        #     print(train["Min"])
+        #     print()
 
         return train_predictions
 
-    def get_device_by_id(self, device_type: DeviceType, device_id: int) -> Device | None:
-        """Return device by device id."""
-        # called by the binary sensors and sensors to get their updated data from self.data
-        try:
-            return [device for device in self.data.devices if device.device_type == device_type and device.device_id == device_id][0]
+    # def get_device_by_id(self, device_type: DeviceType, device_id: int) -> Device | None:
+    #     """Return device by device id."""
+    #     # called by the binary sensors and sensors to get their updated data from self.data
+    #     try:
+    #         return [device for device in self.data.devices if device.device_type == device_type and device.device_id == device_id][0]
 
-        except IndexError:
-            return None
-
-
-class WmataAPI:
-    def __init__(self, api_key):
-        self.api_key: str = api_key
-        self.connected: bool = False
-
-    def validate_api_key(self) -> bool:
-        output = requests.get(
-            headers={"api_key": self.api_key}, url="https://api.wmata.com/Misc/Validate")
-
-        if output.status_code == 200:
-            self.connected = True
-            return True
-        raise APIAuthError("Invalid API key")
-
-    def get_devices(self) -> list[Device]:
-        """Get devices on api."""
-        return [
-            Device(
-                device_id=device.get("id"),
-                device_unique_id=self.get_device_unique_id(
-                    device.get("id"), device.get("type")
-                ),
-                device_type=device.get("type"),
-                name=self.get_device_name(
-                    device.get("id"), device.get("type")),
-                state=self.get_device_value(
-                    device.get("id"), device.get("type")),
-            )
-            for device in DEVICES
-        ]
-
-    def get_device_unique_id(self, device_id: str, device_type: DeviceType) -> str:
-        """Return a unique device id."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return f"{self.controller_name}_D{device_id}"
-        if device_type == DeviceType.TEMP_SENSOR:
-            return f"{self.controller_name}_T{device_id}"
-        return f"{self.controller_name}_Z{device_id}"
-
-    def get_device_name(self, device_id: str, device_type: DeviceType) -> str:
-        """Return the device name."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return f"DoorSensor{device_id}"
-        if device_type == DeviceType.TEMP_SENSOR:
-            return f"TempSensor{device_id}"
-        return f"OtherSensor{device_id}"
-
-    def get_device_value(self, device_id: str, device_type: DeviceType) -> int | bool:
-        """Get device random value."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return choice([True, False])
-        if device_type == DeviceType.TEMP_SENSOR:
-            return randrange(15, 28)
-        return randrange(1, 10)
+    #     except IndexError:
+    #         return None
 
 
 class APIAuthError(Exception):
