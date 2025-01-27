be7524eab83602dedb74680748639b64428bc8c2
Ben Green <bengreenus@gmail.com>
2025-01-26 22:11:29 -0500

    adding line name map to convert line codes to longer names

--- custom_components/wmata/const.py
+++ custom_components/wmata/const.py
@@ -4,4 +4,13 @@
 DEFAULT_SCAN_INTERVAL = 60
 DOMAIN = "WMATA"
 
+LINE_NAME_MAP = {
+    "BL": "Blue",
+    "GR": "Green",
+    "OR": "Orange",
+    "RD": "Red",
+    "SV": "Silver",
+    "YL": "Yellow"
+}
+
 URL = "http://api.wmata.com"
