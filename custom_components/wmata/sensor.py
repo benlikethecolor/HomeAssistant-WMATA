5121842321a3f3a8a1802e2011763f814b66936a
Ben Green <bengreenus@gmail.com>
2025-01-26 22:05:01 -0500

    adding line details as well

--- custom_components/wmata/sensor.py
+++ custom_components/wmata/sensor.py
@@ -15,7 +15,7 @@ class WmataSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, train_index):
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self._attr_name = "Train %s" % (train_index + 1)
+        self._attr_name = f"Train {train_index + 1}"
         self._attr_native_unit_of_measurement = "minutes"
         self.train_index = train_index
 
@@ -24,9 +24,17 @@ class WmataSensor(CoordinatorEntity, SensorEntity):
         """Return the state of the sensor."""
         if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
             next_train = self.coordinator.data.next_trains[self.train_index]["Min"]
-            
             if next_train in ["BRD", "ARR"]:
                 return 0
             else:
                 return next_train
         return None
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attributes = {}
+        if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
+            next_train = self.coordinator.data.next_trains[self.train_index]
+            attributes["train_line"] = next_train["Line"]
+        return attributes
