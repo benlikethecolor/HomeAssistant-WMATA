338c9bdaf3b91c74e5061ee36e0743da33bbf68e
Ben Green <bengreenus@gmail.com>
2025-01-26 21:58:29 -0500

    adding multiple sensors for multiple next trains

--- custom_components/wmata/sensor.py
+++ custom_components/wmata/sensor.py
@@ -6,34 +6,27 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the WMATA sensor platform."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id].coordinator
-    async_add_entities([WmataSensor(coordinator)])
+    async_add_entities([WmataSensor(coordinator, i) for i in range(4)])
 
 
 class WmataSensor(CoordinatorEntity, SensorEntity):
     """Representation of a WMATA sensor."""
 
-    def __init__(self, coordinator):
+    def __init__(self, coordinator, train_index):
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self._attr_name = "WMATA Sensor"
+        self._attr_name = "Train %s" % (train_index + 1)
         self._attr_native_unit_of_measurement = "minutes"
+        self.train_index = train_index
 
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        if self.coordinator.data and self.coordinator.data.next_trains:
-            next_train = self.coordinator.data.next_trains[0]["Min"]
+        if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
+            next_train = self.coordinator.data.next_trains[self.train_index]["Min"]
             
             if next_train in ["BRD", "ARR"]:
                 return 0
-            
             else:
                 return next_train
-            
-            # try:
-            #     # Try to convert the value to an integer
-            #     return int(next_train)
-            # except ValueError:
-            #     # If conversion fails, return the string value
-            #     return next_train
         return None
