08e114bbaf672643dd23321ef8b01c3296a22edc
Ben Green <bengreenus@gmail.com>
2025-01-27 17:55:15 -0500

    adjusting to fix unique ID missing error

--- custom_components/wmata/sensor.py
+++ custom_components/wmata/sensor.py
@@ -10,46 +10,49 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from typing import Any
 
 
-# async def async_setup_entry(hass, config_entry, async_add_entities):
-#     """Set up the WMATA sensor platform."""
-#     coordinator = hass.data[DOMAIN][config_entry.entry_id].coordinator
-#     async_add_entities([WmataSensor(coordinator, i) for i in range(4)])
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+    """Set up the WMATA sensor platform."""
+    runtime_data = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator = runtime_data.coordinator
+
+    sensors = []
+    for i in range(4):
+        sensors.append(WmataSensor(coordinator, i))
+    async_add_entities(sensors, False)
 
 
-# class WmataSensor(CoordinatorEntity, SensorEntity):
-#     """Representation of a WMATA sensor."""
+class WmataSensor(CoordinatorEntity[WmataCoordinator], SensorEntity):
+    """Representation of a WMATA sensor."""
 
-#     def __init__(self, coordinator, train_index):
-#         """Initialize the sensor."""
-#         super().__init__(coordinator)
-#         self._attr_name = f"Train {train_index + 1}"
-#         self._attr_native_unit_of_measurement = "minutes"
-#         self.train_index = train_index
+    def __init__(self, coordinator: WmataCoordinator, train_index: int) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self._attr_name = f"Train {train_index + 1}"
+        self._attr_native_unit_of_measurement = "minutes"
+        self.train_index = train_index
+        self._attr_unique_id = f"{coordinator.unique_id}_train_{train_index + 1}"
 
-#     @property
-#     def native_value(self):
-#         """Return the state of the sensor."""
-#         if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
-#             next_train = self.coordinator.data.next_trains[self.train_index]["Min"]
-#             if next_train in ["BRD", "ARR"]:
-#                 return 0
-#             else:
-#                 return next_train
-#         return None
+    @property
+    def native_value(self):
+        """Return the state of the sensor."""
+        if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
+            next_train = self.coordinator.data.next_trains[self.train_index]["Min"]
+            if next_train in ["BRD", "ARR"]:
+                return 0
+            try:
+                return int(next_train)
+            except ValueError:
+                return None
+        return None
 
-#     @property
-#     def extra_state_attributes(self):
-#         """Return the state attributes."""
-#         attributes = {}
-#         if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
-#             next_train = self.coordinator.data.next_trains[self.train_index]
-#             attributes["line"] = LINE_NAME_MAP[next_train["Line"]]
-#             attributes["destination"] = next_train["Destination"]
-#             attributes["location"] = next_train["LocationName"]
-#             attributes["car"] = next_train["Car"]
-#             attributes["group"] = next_train["Group"]
-            
-#         return attributes
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attributes = {}
+        if self.coordinator.data and len(self.coordinator.data.next_trains) > self.train_index:
+            next_train = self.coordinator.data.next_trains[self.train_index]
+            attributes["train_line"] = next_train.get("Line")
+        return attributes
 
 
 @dataclass
@@ -77,7 +80,7 @@ SENSOR_TYPES: tuple[WmataSensorEntityDescription, ...] = (
         name="Train 1 Line",
         icon="mdi:train",
         state_class=SensorStateClass.TOTAL,
-        value=lambda coord: coord.data.next_trains[0]["Line"],
+        value=lambda coord: LINE_NAME_MAP[coord.data.next_trains[0]["Line"]],
         attributes={},
     ),
     WmataSensorEntityDescription(
@@ -89,46 +92,3 @@ SENSOR_TYPES: tuple[WmataSensorEntityDescription, ...] = (
         attributes={},
     ),
 )
-
-
-async def async_setup_entry(
-        hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
-) -> None:
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-
-    sensors = []
-
-    for description in SENSOR_TYPES:
-        sensors.append(WmataSensor(coordinator, description))
-    async_add_entities(sensors, False)
-
-
-class WmataSensor(
-    CoordinatorEntity[WmataCoordinator], SensorEntity
-):
-    _attr_has_entity_name = True
-    entity_description: WmataSensorEntityDescription
-
-    def __init__(
-            self,
-            coordinator: WmataCoordinator,
-            description: WmataSensorEntityDescription,
-    ) -> None:
-        """Initialize."""
-        super().__init__(coordinator)
-        self._attr_device_info = 'test device info'
-        self._attr_unique_id = f"{coordinator.unique_id}_{DOMAIN}_{description.key}"
-        self.entity_description = description
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        self._attr_native_value = self.entity_description.value(self.coordinator)
-        self.async_write_ha_state()
-        self._attr_extra_state_attributes = self.entity_description.attributes(self.coordinator)
-        self.async_write_ha_state()
-
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        return self.entity_description.value(self.coordinator) is not None
